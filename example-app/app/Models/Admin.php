14359d8bdc6618c5c4dff1c344f98cfd1fa7ca21
Niozk <99679240+Niozk@users.noreply.github.com>
2024-03-24 18:11:39 +0100

    Admin model added
    -Removed password reset token migration
    -Bug fixes

--- example-app/app/Models/Admin.php
+++ example-app/app/Models/Admin.php
@@ -2,19 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
 
-    protected $table = 'users';
+    protected $guard = 'admin';
 
     protected $fillable = [
-        'name', 'email', 'password', 'profile_picture',
+        'name', 'email', 'password',
     ];
 
     protected $hidden = [
